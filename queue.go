c4bc454b7f729c321b3ec7f461c6bb6806b638c1
galifornia <martinho87@gmail.com>
2021-08-17 14:43:44 +0200

    Add Queue

---
+++ queue.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Queue struct {
+	items []int
+}
+
+//enqueue
+func (q *Queue) Enqueue(v int) {
+	q.items = append(q.items, v)
+}
+
+// dequeue
+func (q *Queue) Dequeue() (int, error) {
+	if len(q.items) == 0 {
+		return -1, errors.New("empty queue")
+	}
+
+	p := q.items[0]
+	q.items = q.items[1:len(q.items)]
+	return p, nil
+}
+
+func main() {
+	queue := &Queue{}
+
+	queue.Enqueue(6)
+	queue.Enqueue(743)
+	queue.Enqueue(643)
+	queue.Enqueue(32)
+	queue.Enqueue(7)
+	fmt.Println(queue.items)
+
+	queue.Dequeue()
+	queue.Dequeue()
+	fmt.Println(queue.items)
+}
--- stack.go
+++ stack.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+)
 
 type Stack struct {
 	items []int
@@ -12,30 +14,30 @@ func (s *Stack) Push(v int) {
 }
 
 // pop
-func (s *Stack) Pop() int {
+func (s *Stack) Pop() (int, error) {
 	p := len(s.items) - 1
 	if p < 0 {
-		return -1
+		return -1, errors.New("Stack is empty")
 	}
 	item := s.items[len(s.items)-1]
 	s.items = s.items[:p]
-	return item
+	return item, nil
 }
 
-func main() {
-	stack := &Stack{}
-	stack.Push(44)
-	stack.Push(24)
-	stack.Push(6)
-	stack.Push(36)
+// func main() {
+// 	stack := &Stack{}
+// 	stack.Push(44)
+// 	stack.Push(24)
+// 	stack.Push(6)
+// 	stack.Push(36)
 
-	fmt.Println(stack.items)
+// 	fmt.Println(stack.items)
 
-	stack.Pop()
-	stack.Pop()
-	stack.Pop()
-	stack.Pop()
-	stack.Pop()
-	stack.Pop()
-	fmt.Println(stack.items)
-}
+// 	stack.Pop()
+// 	stack.Pop()
+// 	stack.Pop()
+// 	stack.Pop()
+// 	stack.Pop()
+// 	stack.Pop()
+// 	fmt.Println(stack.items)
+// }
